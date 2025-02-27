52831a3557014eecadfc20d8f1c899a9afd086cf
psafer <piotr.cierpial@outlook.com>
2025-02-28 00:27:04 +0100

    hero

--- cv/src/App.jsx
+++ cv/src/App.jsx
@@ -7,9 +7,17 @@
  * Components
  */
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+      </main>
+    </>
+  );
 };
 
 export default App;
--- cv/src/i18n.js
+++ cv/src/i18n.js
@@ -1,3 +1,7 @@
+/**
+ * @copyright 2025 Piotr Cierpiał
+ * @license Apache-2.0
+ */
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
